aaef7d0d8495763fe1c6a125e9157e1555dca5ad
Hien Nguyen <heinous@bu.edu>
2017-03-30 03:38:06 -0400

    Fixed the duplicate defense

--- Browser-Defense/background.js
+++ Browser-Defense/background.js
@@ -24,15 +24,12 @@ chrome.tabs.getSelected(null, function(tab){
     currentTabId = tab.id;
 });
 
+
 chrome.webRequest.onResponseStarted.addListener(
         function(details) { 
-			//console.log('onBeforeSendHeaders tab id: '+ currentTabId);
-			//console.log('onBeforeSendHeaders: '+ JSON.stringify(details));
 			console.log(details.url);
 			for (var i = 0; i < details.responseHeaders.length; ++i) {
-				//if (details.responseHeaders[i].name === 'Content-Length') {
 				console.log(details.responseHeaders[i].name + ": " + details.responseHeaders[i].value);
-				//}
 			}
 			
 			return {cancel: false}; 
@@ -40,9 +37,70 @@ chrome.webRequest.onResponseStarted.addListener(
         {urls: ["*://www.yandex.com/*","*://yandex.com/*","*://mc.yandex.ru/*","*://suggest-maps.yandex.com/*"], tabId: currentTabId},
         ["responseHeaders"]
 	);
-	
 
+var real_request = "";
+var abort = true;
 
+chrome.webRequest.onBeforeRequest.addListener(
+        function(details) { 
+			if (abort && !details.url.includes("1490724176268")) {
+				real_request = details;
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {message: real_request}, {});
+				});
+				abort = false;
+				return {cancel: true}; 
+			}
+			chrome.runtime.onMessage.addListener(
+				function(request, sender, sendResponse) {
+					if (request.message == "done") {
+						abort = true;
+					}
+				}); 
+			real_request = "";
+			return {cancel: false}; 
+		},
+        {urls: ["*://yandex.com/suggest-spok/*"]},
+        ["blocking","requestBody"]);
 
+var img_request = "";
+var abort_temp = true;
 
+chrome.webRequest.onBeforeRequest.addListener(
+        function(details) { 
+			if (abort_temp && !details.url.includes("1490812290077")) {
+				img_request = details;
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {message_temp: img_request}, {});
+				});
+				abort_temp = false;
+				return {cancel: true}; 
+			}
+			chrome.runtime.onMessage.addListener(
+				function(request, sender, sendResponse) {
+					if (request.message_temp == "done") {
+						abort_temp = true;
+					}
+				}); 
+			img_request = "";
+			return {cancel: false}; 
+		},
+        {urls: ["*://yandex.com/suggest-images/*"]},
+        ["blocking","requestBody"]);
+
+function sleep (seconds) {
+    var start = new Date().getTime();
+    while (new Date() < start + seconds*1000) {}
+    return 0;
+}
+
+function ranchar()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}	
 
--- Browser-Defense/login.js
+++ Browser-Defense/login.js
@@ -2,67 +2,72 @@
 var old_element;
 var new_element;
 var original;
-
-console.log($("#text"));
+var image_original;
 
 $( document ).ready(function() {
-	console.log($(document).find("title").text());
-	if ($(document).find("title").text() == "Yandex") {
-		$("#text").on("keyup", function(event) {
-			original = $("#text").val();
-			
-			old_element = document.getElementById("text");
-			new_element = old_element.cloneNode(true);
-			//old_element.parentNode.replaceChild(new_element, old_element);
-			
-			var query;
-			var repNum = 5;
-			var query_idx = Math.floor(Math.random() * repNum) + 1;
-			
-			for ( var i = 0; i <= 5; i++) {
-				if (i === query_idx) {	
-					query = original;
-					$.get(`https://yandex.com/suggest-spok/suggest-ya.cgi?callback=jQuery183037848249367091813_1490723938382&srv=morda_com_desktop&wiz=TrWth&uil=en&fact=1&v=4&icon=1&hl=1&html=1&n=5&bemjson=1&yu=5530320881490220196&pos=1&part=${query}&_=1490724176268`,
-						function(data){
-							console.log('%c' + data,'color: #ff0000');
-						});
-				} else {
-					query = ranchar();
-					$.get(`https://yandex.com/suggest-spok/suggest-ya.cgi?callback=jQuery183037848249367091813_1490723938382&srv=morda_com_desktop&wiz=TrWth&uil=en&fact=1&v=4&icon=1&hl=1&html=1&n=5&bemjson=1&yu=5530320881490220196&pos=1&part=${query}&_=1490724176268`,
-						function(data){
-							console.log(data);
-						});
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			if (request.message != "") {
+				if ($(document).find("title").text() == "Yandex") {
+					original = $("#text").val();
+					
+					//old_element = document.getElementById("text");
+					//new_element = old_element.cloneNode(true);
+					//parent = old_element.parentNode;
+					var query;
+					var repNum = 5;
+					var query_idx = Math.floor(Math.random() * repNum) + 1;
+				
+					for ( var i = 0; i <= repNum; i++) {
+						if (i === query_idx) {	
+							query = original;
+							$.get(request.message,
+							function(data){
+								console.log('%c' + data,'color: #00ff00');
+							});
+						} else {
+							query = ranchar();
+							$.get(`https://yandex.com/suggest-spok/suggest-ya.cgi?callback=jQuery183037848249367091813_1490723938382&srv=morda_com_desktop&wiz=TrWth&uil=en&fact=1&v=4&icon=1&hl=1&html=1&n=5&bemjson=1&yu=5530320881490220196&pos=1&part=${query}&_=1490724176268`,
+							function(data){
+								console.log(data);
+							});
+						}
+					}
+					chrome.runtime.sendMessage({message: "done"}); 					
 				}
 			}
-		});
-	}
+	});
 	
-	if ($(document).find("title").text().includes("Yandex.Images")) {
-		$('.input__control').on("keyup", function(event) {
-			image_original = $('.input__control').val();
-			
-			var query;
-			var repNum = 5;
-			var query_idx = Math.floor(Math.random() * repNum) + 1;
-			
-			for ( var i = 0; i <= 5; i++) {
-				if (i === query_idx) {	
-					query = image_original;
-					$.get(`https://yandex.com/suggest-images/suggest-ya.cgi?callback=jQuery21403902796222863467_1490812290073&html=1&srv=images_com&yu=5530320881490220196&lr=223&uil=en&latin=1&nl=1&v=4&n=10&portal=1&fact=1&icon=1&hl=1&wiz=TrWth&bemjson=1&part=${query}&pos=2&svg=1&prev-query=&_=1490812290077`,
-						function(data){
-							console.log('%c' + data,'color: #ff0000');
-						});
-				} else {
-					query = ranchar();
-					$.get(`https://yandex.com/suggest-images/suggest-ya.cgi?callback=jQuery21403902796222863467_1490812290073&html=1&srv=images_com&yu=5530320881490220196&lr=223&uil=en&latin=1&nl=1&v=4&n=10&portal=1&fact=1&icon=1&hl=1&wiz=TrWth&bemjson=1&part=${query}&pos=2&svg=1&prev-query=&_=1490812290077`,
-						function(data){
-							console.log(data);
-						});
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			if (request.message_temp != "") {
+				if ($(document).find("title").text().includes("Yandex.Images")) {
+					image_original = $('.input__control').val();
+					var query;
+					var repNum = 5;
+					var query_idx = Math.floor(Math.random() * repNum) + 1;
+				
+					for ( var i = 0; i <= repNum; i++) {
+						if (i === query_idx) {	
+							query = image_original;
+							$.get(request.message_temp,
+							function(data){
+								console.log('%c' + data,'color: #00ff00');
+							});
+							$("#text").val(original); 
+						} else {
+							query = ranchar();
+							$.get(`https://yandex.com/suggest-images/suggest-ya.cgi?callback=jQuery21403902796222863467_1490812290073&html=1&srv=images_com&yu=5530320881490220196&lr=223&uil=en&latin=1&nl=1&v=4&n=10&portal=1&fact=1&icon=1&hl=1&wiz=TrWth&bemjson=1&part=${query}&pos=2&svg=1&prev-query=&_=1490812290077`,
+							function(data){
+								console.log(data);
+							});
+						}
+					}					
 				}
+				chrome.runtime.sendMessage({message_temp: "done"}); 
 			}
-		});
-	}
-});
+	});
+})
 
 function ranchar()
 {
